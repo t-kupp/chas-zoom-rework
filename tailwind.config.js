b95a8b2e9f4241e0ed1c4da6f4f6e67b33540aca
t-kupp <kupper.thorge@gmail.com>
2024-12-10 12:01:23 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#0C5CFF",
+        },
+      },
+    ],
+  },
+};
